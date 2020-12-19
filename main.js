8aea1b4c3c8ebb537e94b2e5cbd1c4014f88da72
unknown <szakalyzs@gmail.com>
2020-12-19 17:19:50 +0100

    Validator object works.

---
+++ main.js
@@ -0,0 +1,16 @@
+'use strict';
+
+const regExp = {
+    rules : {
+        creditCard: /^(\d{4}) \d{4} \d{4} \d{4}$/,
+        ipAdress: /^\d{1,3}.\d{1,3}.\d{1,3}.\d{1,3}$/,
+        macAdress: /^([0-9ABCDEF]{2}:){3}[0-9ABCDEF]{2}$/       //összevontan
+    },
+    validate(text, pattern) {
+        return text.match(this.rules[pattern]) ? true : false;
+    }
+}
+
+console.log(regExp.validate('5412 3661 1708 9901', 'creditCard'));
+console.log(regExp.validate('192.168.0.12', 'ipAdress'));
+console.log(regExp.validate('A0:FF:5D:88', 'macAdress'));
